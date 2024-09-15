e77589a5448616642c0dce2a6b2a488074dd55ae
AlexisofAcademy <160819618+AlexisofAcademy@users.noreply.github.com>
2024-09-15 16:52:13 -0600

    Add files via upload

---
+++ Polymorphism.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            List<IQuittable> Employee = new List<IQuittable>();
+
+                Cain cain = new Cain();
+
+                Employee.Add(Cain);
+
+                foreach (IQuittable employee in Employee)
+                {
+                    Employee.Quit();
+                }
+        }
+       // Create an interface called IQuittable and have it define a void method called Quit().
+       public interface IQuittable 
+       {
+        void Quit();
+
+        int legs { get; set; }
+       }
+
+       // Have your Employee class from the previous drill inherit that interface and implement the Quit() method in any way you choose.
+        public class Cain : IQuittable
+        {
+            public int legs { get; set; }
+
+            public void Quit()
+            {
+                Console.WriteLine("Cain is quit");
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       
+    
+
