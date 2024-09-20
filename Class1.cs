e452b24d0e77b256f8e620ac336197ed9697c0c7
AlexisofAcademy <160819618+AlexisofAcademy@users.noreply.github.com>
2024-09-19 23:39:34 -0600

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Create a class 
+namespace Class1
+{
+//In that class, create a void method that takes two integers as parameters. Have the method do a math operation on the first integer and display the second integer to the screen. 
+    public class Class1
+    {
+        int a;
+
+        public int sum(int c, int b)
+        {
+            a = c + b;
+            return i;
+        }
+s
+       // Call the method in the class, specifying the parameters by name.
+        public void Display()
+        {
+           MessageBox.Show("Hello Alexis");
+        }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collection.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+// In the Main() method of the console app, instantiate the class.
+namespace ConsoleApplication_Class1
+{
+    class Program 
+       //Call the method in the class, passing in two numbers.
+       static void Main(string[] args)
+       {
+          Class1 ob = new Class1();
+
+          Console.Write("Sum = " + ob.sum(56, 23));
+          ob.Display();
+          Console.Read();
+
+       }
+
+
+
+
+
+}
