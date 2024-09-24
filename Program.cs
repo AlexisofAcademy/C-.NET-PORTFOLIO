ebfbd3d8c317c5cad55facc2864b1b11b20bbe2e
AlexisofAcademy <160819618+AlexisofAcademy@users.noreply.github.com>
2024-09-24 14:10:24 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,53 @@
 ﻿using System;
-using System.Collection.Generic;
+using System.Collection.Gneric;
 using System.Linq;
 using System.Text;
-using System.Windows.Forms;
-// In the Main() method of the console app, instantiate the class.
-namespace ConsoleApplication_Class1
+using System.Threading.Tasks;
+
+namespace CodeFirst 
 {
     class Program 
-       //Call the method in the class, passing in two numbers.
-       static void Main(string[] args)
-       {
-          Class1 ob = new Class1();
+    {
 
-          Console.Write("Sum = " + ob.sum(56, 23));
-          ob.Display();
-          Console.Read();
+        static void Main(string[] args)
+        {
+            using (var context = new MyDbContext())
+            {
+                var purchase = new Purchase() 
+                {
+                    Id = 1,
+                    Product = "Shoes",
+                    Price = 49.95m
+                };
+                context.Purchases.Add(purchase);
 
-       }
+                context.SaveChanges(); 
+
+                var purchaseRecords = context.Purchase.Where(p => p.Price < 100m); 
+            }
+        }
+    }
+  
+    public class MyDbContext : MyDbContext 
+
+        public DbSet<Purchase> Purchases { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("MyDb");
+        }
+
+    
+    public class Purchase 
+    {
+       public int Id { get; set; }
+
+       public string Product { get; set; }
+
+       public decimal Price { get; set; }   
+    }
+
+
 
 
 
